c9256c096e39d420702188d2a625dde6c99e9963
Uka0001 <ukraine0001@gmail.com>
2022-09-10 22:44:02 +0300

    leet code tasks completed

---
+++ src/main/java/leetcode/programming/skills1/BinarySearch.java
@@ -0,0 +1,29 @@
+package leetcode.programming.skills1;
+
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        return recursive(nums, low, high, target);
+    }
+    public int recursive(int[] nums, int low, int high, int target) {
+        if(low > high) return -1;
+        int mid = (low + high) / 2;
+
+        if(nums[mid] == target) return mid;
+
+        if(target > nums[mid]) return recursive(nums, mid + 1, high, target);
+        else return recursive(nums, low, mid - 1, target);
+    }
+    //alternative decision from leet code;
+    /*int n = array.length;
+        int low = 0;
+        int high = n-1;
+        while(low<=high) {
+            int mid = low + high >> 1;
+            if(array[mid] == v) return mid;
+            if(array[mid] < v) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;*/
+}
