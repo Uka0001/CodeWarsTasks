8912a1e3b71b1044b77ccc2b98fa5dbadf17ba6c
Uka0001 <ukraine0001@gmail.com>
2022-10-22 00:05:23 +0300

    remove letter completed

--- src/main/java/org/example/RemoveLetter.java
+++ src/main/java/org/example/RemoveLetter.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class RemoveLetter {
     public static void main(String[] args) {
-        System.out.println(removeLetter("Ґудзик").toString());
+        System.out.println(removeLetter("Reptile"));
     }
 
     static char [] removeLetter (String input){
@@ -11,6 +11,11 @@ public class RemoveLetter {
             charArray = new char[0];
         } else {
             charArray = input.toLowerCase().toCharArray();
+            for (int i = 0; i < charArray.length; i++){
+                if (charArray[i] == 'r'){
+                    charArray[i] = '!';
+                }
+            }
 
         }
         return charArray;
